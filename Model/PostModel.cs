5fff52b3e0d09bf97e117badd3e023fc4fafbbae
MmeGGy <47536975+MmeGGy@users.noreply.github.com>
2022-12-01 13:40:22 +0200

    Add files via upload

---
+++ Model/PostModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APITest.Model
+{
+    public class PostModel
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public int totalprice { get; set; }
+        public bool depositpaid { get; set; }
+        public string checkin { get; set; }
+        public string checkout { get; set; }
+        public string additionalneeds { get; set; }
+    }
+}
